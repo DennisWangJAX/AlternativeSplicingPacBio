174ad8bd628846beddad10148c146e66f0c5acaf
Qianchang Dennis Wang <qianchang.wang@jax.org>
2018-02-21 21:36:04 -0500

    Add files via upload

---
+++ statistics/intron_filter_by_gff.R
@@ -0,0 +1,36 @@
+intron_filter_by_gff <- function(gff){
+  source("https://bioconductor.org/biocLite.R")
+  require("stringr"); require("rtracklayer"); require("GenomicRanges")
+
+  annot_gr = import.gff(gff)
+  gene_names = unique(annot_gr[!is.na(annot_gr$gffcompare_gene_name)]$gffcompare_gene_name)
+  stat_prefiltered = NULL
+  stat_remain = NULL
+  stat_filtered = NULL
+  analyzed_gene = NULL
+  count_skipped = 0
+
+  for(i in 1:(length(gene_names))){
+  #for(i in 1:10){
+    gene = gene_names[i]
+    gr_temp_inter = annot_gr[annot_gr$gffcompare_gene_name %in% gene]
+    temp_gene_id = unique(gr_temp_inter$gene_id)[1]
+    gr_temp = annot_gr[annot_gr$gene_id == temp_gene_id]
+    temp_gr_exon = gr_temp[gr_temp$type == "exon"]
+    transcript_count = length(unique(gr_temp$transcript_id))
+    if (transcript_count > 1 & length(temp_gr_exon) > 2*transcript_count ){
+      print(paste("Analyzing ", gene))
+      analyzed_gene <- c(analyzed_gene, gene)
+      #temp_data <- intron_filter_by_GRanges(gr_temp)
+      #stat_prefiltered <- c(stat_prefiltered, temp_data$prefiltered_new_exon_count)
+      #stat_remain <- c(stat_remain, temp_data$remain_count)
+      #stat_filtered <- c(stat_filtered, temp_data$filtered_count)
+    }
+
+    else{count_skipped <- count_skipped + 1}
+  }
+
+  print(paste("Total Skipped Genes: ", count_skipped))
+  list("stat_prefiltered" = stat_prefiltered, "stat_remain" = stat_remain, "stat_filtered" = stat_filtered,
+       "analyzed_gene" = analyzed_gene)
+}
