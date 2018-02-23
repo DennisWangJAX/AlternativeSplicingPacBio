850250257321a4ee0ef13fdd234d91eadc6ece15
Qianchang Dennis Wang <qianchang.wang@jax.org>
2018-02-23 16:36:09 -0500

    Create quickplot.R

---
+++ Plot/quickplot.R
@@ -0,0 +1,5 @@
+gff_name = "RIPK1.gtf"
+results <- intron_filter_by_GRanges(import.gff(gff_name))
+group_result <- isoform_group(results$remain_binary, results$remain)
+#visualize_cluster(gff_name, group_result$group_index, results$remain)
+visualize(gff_name, 1, transcript_name = group_result$isoform_group[[1]])
